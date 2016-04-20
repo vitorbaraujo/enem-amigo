376a28cbe5bef9093ee83a80016262c4bca21846
Renata Soares <renata.sds09@gmail.com>
2016-04-19 23:54:07 -0300

    Adding coments and returns to methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,44 @@
 class SessionsController < ApplicationController
+
+  # name: new
+  # explanation: set home_page object as true
+  # parameters:
+  # -none
+  # return: home_page object
+
   def new
-    @home_page = true
+    return @home_page = true
   end
+
+  # name: create
+  # explanation: log in 
+  # parameters:
+  # -none
+  # return: message of sucess or error
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path
-      flash[:success] = "Logado com sucesso!"
+      return flash[:success] = "Logado com sucesso!"
     else
-      flash.now[:danger] = 'Combinação inválida de e-mail/senha'
+      return flash.now[:danger] = 'Combinação inválida de e-mail/senha'
       render 'new'
     end
   end
+
+  # name: destroy
+  # explanation: log out
+  # parameters:
+  # -none
+  # return: login_path
 
   def destroy
     if current_user
       log_out
     end
-    redirect_to login_path
+    return redirect_to login_path
   end
 
 end
