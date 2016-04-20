6fd4c18ae6744a2633b1b72de7717b20fea7d527
Renata Soares <renata.sds09@gmail.com>
2016-04-19 23:15:07 -0300

    refactoring models adding comments to methods and fixing identation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,19 @@
 class Comment < ActiveRecord::Base
+	
   serialize :user_ratings, Array
   validates :content, presence: true
 
   belongs_to :post
   belongs_to :user
 
-  def count_comment_rates
+  # name: count_comment_rates
+  # explanation: this method counts user ratings
+  # parameters:
+  # -none
+  # return: amout of user ratings
+
+	def count_comment_rates
     self.user_ratings.count
   end
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
+	
 	validates :name, presence: true, uniqueness: true
 	validates :description, presence: true, uniqueness: true
 
