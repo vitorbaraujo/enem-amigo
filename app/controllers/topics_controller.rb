de68386fe300d41ac0a8a876c40dea9ddc20359f
Renata Soares <renata.sds09@gmail.com>
2016-04-20 18:29:01 -0300

    Fixing identation, putting comments on helpers and creating constants

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -24,14 +24,12 @@ class TopicsController < ApplicationController
 
 	def create
 		@topic = Topic.new(topic_params)
-
 		if @topic.save
 			flash[:success] = "Tópico criado com sucesso"
-			return redirect_to (@topic)
+			return redirect_to(@topic)
 		else
       #nothing to do
     end
-    
 	end
 
   # name: show
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,5 @@ class Topic < ActiveRecord::Base
 	validates :description, presence: true, uniqueness: true
 
 	has_many :posts
+	
 end
