74b9f233c2021bf8619ba23d6995e061f31a0681
Victor Navarro <victor.matias.navarro@gmail.com>
2016-04-20 22:15:46 -0300

    Adjusting models to identation, variable names, if and else and method arguments rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ActiveRecord::Base
   has_many :active_battles, class_name: 'Battle', foreign_key: 'player_1_id'
   has_many :passive_battles, class_name: 'Battle', foreign_key: 'player_2_id'
   has_many :won_battles, class_name: 'Battle', foreign_key: 'winner'
-
   has_many :notifications
   has_and_belongs_to_many :medals
 
   serialize :accepted_questions, Array
-  serialize :tried_questions, Array
   serialize :answered_exams, Array
+  serialize :tried_questions, Array
   serialize :exam_performance, Array
 
   before_save { self.email = email.downcase }
@@ -30,28 +29,35 @@ class User < ActiveRecord::Base
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-    BCrypt::Password.log_user_in(string, cost: cost)
+    BCrypt::Password.create(string, cost: cost)
   end
 
   def count_questions_by_area(area)
-    i = 0
+    right_questions_in_area = 0
     self.accepted_questions.each do |t|
       if Question.find(t).area == area
-        i = i+1
+        right_questions_in_area = right_questions_in_area+1
+      else
+        # nothing to do
       end
     end
-    i
+    return right_questions_in_area
   end
 
   def find_position_in_ranking
     ranking = User.all.order(:points).reverse
     for i in 0...ranking.count
-      return i+1 if ranking[i].id == self.id
+      if ranking[i].id == self.id
+        return i+1
+      else
+        # nothing to do
+      end
     end
   end
 
   def total_accepted_questions
-    self.accepted_questions.count
+    all_right_questions = self.accepted_questions.count
+    return all_right_questions
   end
 
   def data
@@ -64,41 +70,59 @@ class User < ActiveRecord::Base
   end
 
   def sum_exam_performance
-    self.exam_performance.empty? ? 0 : self.exam_performance.inject(:+)
+    if self.exam_performance.empty?
+      return 0
+    else
+      return self.exam_performance.inject(:+)
+    end
   end
 
   def progress
-      (100 * self.total_accepted_questions.to_f/Question.all.count).round(2)
+    progress_in_system = (100 * self.total_accepted_questions.to_f/Question.all.count).round(2)
+    return progress_in_system
   end
 
   def battles
-    (self.active_battles + self.passive_battles).sort
+    return (self.active_battles + self.passive_battles).sort
   end
 
-  def average_performance area
+  def average_performance(area)
     accepted_questions = []
     tried_questions = []
     self.accepted_questions.each do |question_id|
-      q = Question.find question_id
-      accepted_questions << q if q.area == area
+      question_found = Question.find(question_id)
+      if question_found.area == area
+        accepted_questions << question_found
+      else
+        # nothing to do
+      end
     end
+
     self.tried_questions.each do |question_id|
-      q = Question.find question_id
-      tried_questions << q if q.area == area
+      question_found = Question.find(question_id)
+      if question_found.area == area
+        tried_questions << question_found
+      else
+        # nothing to do
+      end
     end
     result = accepted_questions.count.to_f / tried_questions.count
-    result.nan? ? 0.0 : result
+    if result.nan?
+      return 0.0
+    else
+      return result
+    end
   end
 
-  def classification area
+  def classification(area)
     performance = self.average_performance area
     performance *= 100
     if performance >= 0 && performance <= 30
-      "beginner"
+     return "beginner"
     elsif performance > 30 && performance <= 60
-      "intermediate"
+     return "intermediate"
     else
-      "advanced"
+     return "advanced"
     end
   end
 
