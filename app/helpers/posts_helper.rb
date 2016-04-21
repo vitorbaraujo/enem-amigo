03146c32e85984deef3695eae9ac271c8892e18d
jessicacris <jessica.cris1127@gmail.com>
2016-04-21 00:19:54 -0300

    Fixing identation, putting comments on helpers and remove unless

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,10 @@
 module PostsHelper
 
+ # name: new_post
+ # explanation: this method list an new post
+ # parameters:
+ # -none
+ # return: post
 	def new_post
 		@post = Post.new
 	end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,29 @@
 module UsersHelper
 
+ # name: new_post
+ # explanation: this method the ten users with the highest score
+ # parameters:
+ # -none
+ #return: ranking
   def top10
     ranking = User.all.order(:points).reverse
     @top10 = ranking.take(10)
   end
 
-  #returns level of user without updating it
+ # name: new_post
+ # explanation: this method level of user without updating it
+ # parameters:
+ # -none
+ #return: level
   def find_level user_points
     @user_level = (Math.sqrt 2*user_points).to_i
   end
 
-  #update user level
+ # name: new_post
+ # explanation: this method update user level
+ # parameters:
+ # -none
+ #return: level 
   def update_user_level
     current_user.update_attribute(:level,@user_level)
     @user_level = current_user.level
