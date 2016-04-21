cfae0ef5729c84db999742803c41b48ef32a149e
jessicacris <jessica.cris1127@gmail.com>
2016-04-21 00:14:17 -0300

    Adding comments and if else and applying style and design

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,25 +23,70 @@ class Question < ActiveRecord::Base
 
   ALTERNATIVES_COUNT = 5
 
+ # name: hit_rate
+ # explanation: this method calculates hit rate
+ # parameters:
+ # -none
+ # return: hit_rateh
   def hit_rate
-    hit_rate = self.tries == 0 ? 0 : (100 * (self.hits.to_f / self.tries)).round(2)
+    if self.tries == 0 
+      hit_rate ||= 0
+    else
+      hit_rate ||= (100 * (self.hits.to_f / self.tries)).round(2)
+    end 
+    return hit_rate
   end
 
+ # name: next_questions
+ # explanation: this method redirects to next question 
+ # parameters:
+ # -none
+ # return: question
   def next_question
-    question = Question.where(area: self.area).where("id > ?", id).first
-    question ? question : Question.where(area: self.area).first
+    question ||= Question.where(area: self.area).where("id > ?", id).first
+    if !question
+      question = Question.where(area: self.area).first
+    else
+      # nothing to do
+    end
+      return question 
   end
 
+ # name: users_hit_rate
+ # explanation: this method calculates hit rate of user 
+ # parameters:
+ # -none
+ # return: users_hit_rate
   def users_hit_rate
-    users_hit_rate = self.users_tries == 0 ? 0 :
-          (100 * (self.users_hits.to_f / self.users_tries)).round(2)
+    if self.users_tries == 0
+      users_hit_rate ||= 0
+    else
+      users_hit_rate ||= (100 * (self.users_hits.to_f / self.users_tries)).round(2)
+    end
+      return users_hit_rate
   end
 
+
+ # name: total_hit_rate
+ # explanation: this method calculates total hit rate 
+ # parameters:
+ # -none
+ # return: total_hit_rate
   def total_hit_rate
     total_hit_rate = (100 * (self.hits + self.users_hits.to_f) / (self.tries + self.users_tries)).round(2)
-    total_hit_rate = total_hit_rate.nan? ? 0.0 : total_hit_rate
+    if total_hit_rate.nan?
+        total_hit_rate ||= 0.0
+    else
+      # nothing to do
+    end
+    return total_hit_rate
   end
 
+ # name: data
+ # explanation: this method return an hash with data for comparison
+ # parameters:
+ # -none
+ # return:
   def data
     [
       {"name" => "Enem","data" => {"Enem" => self.hit_rate}},
@@ -51,6 +96,11 @@ class Question < ActiveRecord::Base
   end
 
   class << self
+ # name: method_missing
+ # explanation: this method calculates hit rate
+ # parameters:
+ # -none
+ # return: question
     def method_missing method_name, *args
       method_name = method_name.to_s
       if method_name.slice! /_questions/
@@ -67,13 +117,25 @@ class Question < ActiveRecord::Base
 
   private
 
+ # name: alternatives_count_valid?
+ # explanation: this method validates amount of alternatives 
+ # parameters:
+ # -none
+ # return: alternatives
     def alternatives_count_valid?
       self.alternatives.size == ALTERNATIVES_COUNT
     end
 
+ # name: check_alternatives_number
+ # explanation: this method check number of alternatives
+ # parameters:
+ # -none
+ # return: alternatives
     def check_alternatives_number
-      unless alternatives_count_valid?
+      if !alternatives_count_valid?
         errors.add(:alternatives, "cannot be less than 5")
+      else
+        # nothing to do
       end
     end
 end
