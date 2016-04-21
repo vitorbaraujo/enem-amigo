9d77ddb5a8056dc072f0f5ae82b8ffe88491df3e
jessicacris <jessica.cris1127@gmail.com>
2016-04-21 00:19:54 -0300

    Fixing identation, putting comments on helpers and remove unless

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,37 +1,89 @@
 module SessionsHelper
 
+ # name: log_in
+ # explanation: this method check if user logged 
+ # parameters:
+ # -none
+ #return: user
   def log_in(user)
-    session[:user_id] = user.id
+    session[:user_id] ||= user.id
   end
 
+ # name: current_user
+ # explanation: this method level of user without updating it
+ # parameters:
+ # -none
+ #return: current_user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+ # name: logged_in
+ # explanation: this method check if user logged 
+ # parameters:
+ # -none
+ #return: logged
   def logged_in?
     !current_user.nil?
   end
 
+ # name: log_out
+ # explanation: this method check if user log out
+ # parameters:
+ # -none
+ #return: log_out
   def log_out
     session.delete(:user_id)
-    @current_user = nil
+    @current_user ||= nil
   end
 
+ # name: authenticate_user
+ # explanation: this method check the authenticate of user
+ # parameters:
+ # -none
+ #return: redirect_to login_path 
   def authenticate_user
-    redirect_to login_path unless logged_in?
+    if !logged_in?
+      return redirect_to login_path 
+    else
+      # nothing to do
+    end
   end
 
+ # name: authenticate_admin
+ # explanation: this method check the authenticate of admin
+ # parameters:
+ # -none
+ #return: authenticate_admin
   def authenticate_admin
-    redirect_to_back(root_path) unless current_user.role_admin?
+    if !current_user.role_admin?
+      redirect_to_back(root_path) 
+    else
+      # nothing to do
+    end
   end
 
+ # name: verify_user_permission
+ # explanation: this method the veriry permission of user
+ # parameters:
+ # -none
+ #return: user
   def verify_user_permission
-    user = User.find(params[:id])
-    redirect_to_back(users_path) unless user.id == current_user.id or current_user.role_admin?
+    user ||= User.find(params[:id])
+    if !(user.id == current_user.id || current_user.role_admin?)
+      redirect_to_back(users_path)
+    else
+      # nothing to do
+    end 
   end
 
+ # name: redirect_to_back
+ # explanation: this method the redirect to back
+ # parameters:
+ # -none
+ #return: 
   def redirect_to_back(default = root_path)
-    if !request.env["HTTP_REFERER"].blank? and request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
+    if !request.env["HTTP_REFERER"].blank? && request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
       redirect_to :back
     else
       redirect_to default
