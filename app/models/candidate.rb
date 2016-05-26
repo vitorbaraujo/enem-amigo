4f8e7f61c8aae4a0850103a177450b7298a12a64
Renata Soares <renata.sds09@gmail.com>
2016-05-26 10:18:49 -0300

    Adding file comment and class comment in candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,9 @@
+# File: candidate.rb
+# Purpose: Implementation of candidate's class 
+# License : LGPL. No copyright.
+
+# Candidate represents the students that make ENEM test.
+
 class Candidate < ActiveRecord::Base
 	
 	validates :general_average, presence: true
