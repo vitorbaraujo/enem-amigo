2f47715b18ffc4a3c52de046c2e669b1130a6448
Victor Navarro <victor.matias.navarro@gmail.com>
2016-05-26 21:37:56 -0300

    Adding file and class comments to notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,10 @@
+# File: notification.rb
+# Purpose: Implementation of notification's class, to maintain relations to
+# battle and users classes
+# License : LGPL. No copyright.
+
+# Notifications are the way for the system to show new battles between users
+
 class Notification < ActiveRecord::Base
 
   belongs_to :battle
