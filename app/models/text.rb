f137461b6a0e27c627c079a40441aeae889c925a
Vítor Barbosa <vitornga15@gmail.com>
2016-05-28 22:51:59 -0300

    Adding file comments to text model

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,3 +1,10 @@
+# File: text.rb
+# Purpose: Implementation of the class Text
+# License: LGPL. No copyright.
+
+# This class contains attributes to parse the json information from the question
+# database into the system
+
 class Text < ActiveRecord::Base
 
   belongs_to :question
