fff7412793f14e1a486bb91882e5bb63f2793350
jessicacris <jessica.cris1127@gmail.com>
2016-06-03 10:04:19 -0300

    Adding file comments to exam model

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,3 +1,9 @@
+# File: exam.rb
+# Purpose: Maintain logic for exam.
+# License: LGPL. No copyright.
+
+# This model lists the questions that were answered by exam users.
+
 class Exam < ActiveRecord::Base
 
   serialize :questions, Array
