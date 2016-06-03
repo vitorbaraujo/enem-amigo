f0afc374ce731487f1b54fbe481c4826330687b5
Renata Soares <renata.sds09@gmail.com>
2016-06-03 17:26:04 -0300

    Adding file and class comment to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
+# File: sessions_controller.rb
+# Purpose: Controller of sessions. 
+# License : LGPL. No copyright.
+
+# This controller contains the logic involved 
+# to log user in and out of the application
+# and creates home page.
+
 class SessionsController < ApplicationController
 
   # name: new
@@ -7,45 +15,40 @@ class SessionsController < ApplicationController
   # return: home_page object
 
   def new
-    @home_page = true
-    return @home_page
+    return @home_page = true
   end
 
   # name: log_user_in
-  # explanation: log user into enem amigo
+  # explanation: log in 
   # parameters:
   # -none
   # return: message of sucess or render new
 
   def log_user_in
     user = User.find_by(email: params[:session][:email].downcase)
-
     if user && user.authenticate(params[:session][:password])
-      log_in (user)
-      redirect_to (root_path)
+      log_in(user)
+      redirect_to(root_path)
       return flash[:success] = "Logado com sucesso!"
     else
       flash.now[:danger] = 'Combinação inválida de e-mail/senha'
-      render ('new')
+      render('new')
     end
-
   end
 
   # name: log_user_out
-  # explanation: log user out of enem amigo
+  # explanation: log out
   # parameters:
   # -none
   # return: login_path
 
   def log_user_out
-
     if current_user
       log_out
     else
       #nothing to do
     end
-    
-    return redirect_to (login_path)
+    return redirect_to(login_path)
   end
 
 end
